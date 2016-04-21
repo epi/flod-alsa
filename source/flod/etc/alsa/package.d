081bbf5d4a2cb852a60c54714cb66c8d40f6da74
Adrian Matoga <epi@atari8.info>
2016-04-21 22:38:54 +0200

    Use alias Context and new attributes, public pipe playPcm, refactor.
    PCM parameters are now set in private member func setParams.

--- source/flod/etc/alsa/package.d
+++ source/flod/etc/alsa/package.d
@@ -2,11 +2,16 @@ module flod.etc.alsa;
 
 import deimos.alsa.pcm;
 import flod.meta : NonCopyable;
-import flod.traits : pushSink;
+import flod.traits : sink, Method;
+import flod.pipeline;
 
-@pushSink!ubyte
-static struct AlsaPcm {
+@sink!ubyte(Method.push)
+private struct AlsaPcm(alias Context, A...) {
 	mixin NonCopyable;
+	mixin Context!A;
+
+	private snd_pcm_t* hpcm;
+	private int bytesPerSample;
 
 	this(uint channels, uint samplesPerSec, uint bitsPerSample)
 	{
@@ -14,13 +19,7 @@ static struct AlsaPcm {
 		int err;
 		if ((err = snd_pcm_open(&hpcm, "default".toStringz(), snd_pcm_stream_t.PLAYBACK, 0)) < 0)
 			throw new Exception("Cannot open default audio device");
-		if ((err = snd_pcm_set_params(
-			hpcm, bitsPerSample == 8 ? snd_pcm_format_t.U8 : snd_pcm_format_t.S16_LE,
-			snd_pcm_access_t.RW_INTERLEAVED,
-			channels, samplesPerSec, 1, 100000)) < 0) {
-			close();
-			throw new Exception("Cannot set audio device params");
-		}
+		setParams(channels, samplesPerSec, bitsPerSample);
 		bytesPerSample = bitsPerSample / 8 * channels;
 	}
 
@@ -29,7 +28,19 @@ static struct AlsaPcm {
 		close();
 	}
 
-	void close()
+	private void setParams(uint channels, uint samplesPerSec, uint bitsPerSample)
+	{
+		int err;
+		if ((err = snd_pcm_set_params(
+			hpcm, bitsPerSample == 8 ? snd_pcm_format_t.U8 : snd_pcm_format_t.S16_LE,
+			snd_pcm_access_t.RW_INTERLEAVED,
+			channels, samplesPerSec, 1, 100000)) < 0) {
+			close();
+			throw new Exception("Cannot set audio device params");
+		}
+	}
+
+	private void close()
 	{
 		if (hpcm is null)
 			return;
@@ -47,20 +58,20 @@ static struct AlsaPcm {
 		}
 		return buf.length;
 	}
-
-private:
-	snd_pcm_t* hpcm;
-	int bytesPerSample;
 }
 
-unittest
+auto playPcm(S, Args...)(auto ref S schema, auto ref Args args)
+	if (isSchema!S)
 {
+	return schema.pipe!AlsaPcm(args);
+}
+
+unittest {
 	// play 1 second of 1 kHz sine wave
 	import std.math : sin, PI;
 	import std.range : iota, cycle, take, array;
 	import std.algorithm : map;
-	auto pcm = AlsaPcm(1, 8000, 8);
 	auto buf = iota(8).map!(i => cast(ubyte) (sin(PI * i / 4.0) * 127 + 128))
 		.cycle.take(8000).array();
-	pcm.push(buf);
+	buf.playPcm(1, 8000, 8);
 }
